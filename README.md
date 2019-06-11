49d4512a023ccc67cba4aad23b3e95e1905d2f3e
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-06-11 18:56:35 -0300

    :tada: Initial Commit

---
+++ README.md
@@ -0,0 +1,81 @@
+
+# rn-user-defaults
+
+
+Use `UserDefaults` (iOS) with React Native.
+All data are stored as String, in case more complex data structure is needed, serialize it (e.g. JSON).
+
+## Getting started
+
+`$ npm install rn-user-defaults --save`
+
+### Mostly automatic installation
+
+`$ react-native link rn-user-defaults`
+
+### Manual installation
+
+
+#### iOS
+
+1. In XCode, in the project navigator, right click `Libraries` ➜ `Add Files to [your project's name]`
+2. Go to `node_modules` ➜ `rn-user-defaults` and add `RNUserDefaults.xcodeproj`
+3. In XCode, in the project navigator, select your project. Add `libRNUserDefaults.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
+4. Run your project (`Cmd+R`)<
+
+#### Android
+
+1. Open up `android/app/src/main/java/[...]/MainApplication.java`
+  - Add `import com.djorkaeffalexandre.rn_user_defaults.RNUserDefaultsPackage;` to the imports at the top of the file
+  - Add `new RNUserDefaultsPackage()` to the list returned by the `getPackages()` method
+2. Append the following lines to `android/settings.gradle`:
+  	```
+  	include ':rn-user-defaults'
+  	project(':rn-user-defaults').projectDir = new File(rootProject.projectDir, 	'../node_modules/rn-user-defaults/android')
+  	```
+3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
+  	```
+      compile project(':rn-user-defaults')
+  	```
+
+## Usage
+```javascript
+import DefaultPreference from 'rn-user-defaults';
+
+DefaultPreference.get('key').then(function(value) {console.log(value)});
+DefaultPreference.set('key', 'value').then(function() {console.log('done')});
+```
+
+## API
+
+```haxe
+function get(key:String):Promise<String>;
+function set(key:String, value:String):Promise<Void>;
+function clear(key:String):Promise<Void>;
+function getMultiple(keys:Array<String>):Promise<Array<String>>;
+function setMultiple(data:Object):Promise<Void>;
+function clearMultiple(keys:Array<String>):Promise<Void>;
+function getAll():Promise<Object>;
+function clearAll():Promise<Void>;
+
+/** Gets and sets the current user default suite name (ios) **/
+function getName():Promise<String>;
+function setName(name:String):Promise<Void>;
+```
+
+## Cordova Native Storage Compatibility
+This module is compatible with cordova-plugin-native-storage.
+
+### Android
+You need to use the same SharedPreference as in the cordova plugin, to do so add
+the following line:
+
+```js
+import { Platform } from 'react-native';
+// ...
+if (Platform.OS === 'android') DefaultPreference.setName('NativeStorage');
+```
+
+### iOS
+You don't need to change the name, as cordova-plugin-native-storage uses the default
+value.
---
+++ ios/RNUserDefaults.m
@@ -0,0 +1,113 @@
+
+#import "RNUserDefaults.h"
+
+@implementation RNUserDefaults
+
+NSString* defaultSuiteName = nil;
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+- (NSUserDefaults *) getDefaultUser{
+    if(defaultSuiteName == nil){
+        NSLog(@"No prefer suite for userDefaults. Using standard one.");
+        return [NSUserDefaults standardUserDefaults];
+    } else {
+        NSLog(@"Using %@ suite for userDefaults", defaultSuiteName);
+        return [[NSUserDefaults alloc] initWithSuiteName:defaultSuiteName];
+    }
+}
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(setName:(NSString *)name
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    defaultSuiteName = name;
+    resolve([NSNull null]);
+}
+
+RCT_EXPORT_METHOD(getName:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    resolve(defaultSuiteName);
+}
+
+RCT_EXPORT_METHOD(get:(NSString *)key
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    resolve([[self getDefaultUser] stringForKey:key]);
+}
+
+RCT_EXPORT_METHOD(set:(NSString *)key value:(NSString *)value
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    [[self getDefaultUser] setObject:value forKey:key];
+    resolve([NSNull null]);
+}
+
+RCT_EXPORT_METHOD(clear:(NSString *)key
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    [[self getDefaultUser] removeObjectForKey:key];
+    resolve([NSNull null]);
+}
+
+RCT_EXPORT_METHOD(getMultiple:(NSArray *)keys
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    NSMutableArray *result = [NSMutableArray array];
+    for(NSString *key in keys) {
+        NSString *value = [[self getDefaultUser] stringForKey:key];
+        [result addObject: value == nil ? [NSNull null] : value];
+    }
+    resolve(result);
+}
+
+RCT_EXPORT_METHOD(setMultiple:(NSDictionary *)data
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    for (id key in data) {
+        [[self getDefaultUser] setObject:[data objectForKey:key] forKey:key];
+    }
+    resolve([NSNull null]);
+}
+
+RCT_EXPORT_METHOD(clearMultiple:(NSArray *)keys
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    for(NSString *key in keys) {
+        [[self getDefaultUser] removeObjectForKey:key];
+    }
+    resolve([NSNull null]);
+}
+
+RCT_EXPORT_METHOD(getAll:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    NSArray *keys = [[[self getDefaultUser] dictionaryRepresentation] allKeys];
+    NSMutableDictionary *result = [NSMutableDictionary dictionary];
+    for(NSString *key in keys) {
+        NSString *value = [[self getDefaultUser] stringForKey:key];
+        result[key] = value == nil ? [NSNull null] : value;
+    }
+    resolve(result);
+}
+
+RCT_EXPORT_METHOD(clearAll:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
+    NSArray *keys = [[[self getDefaultUser] dictionaryRepresentation] allKeys];
+    [self clearMultiple:keys resolve:resolve reject:reject];
+}
+
+@end
